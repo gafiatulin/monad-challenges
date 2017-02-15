714bdbcefd6176ad39e5a13deef0e139e0777bb0
Victor Gafiatulin <gafiatulin@gmail.com>
2017-02-16 01:05:09 +0300

    refactor set1
    Adopt Code Template

---
+++ src/Set1.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RebindableSyntax  #-}
+
+module Set1 where
+
+import MCPrelude
+-- import Control.Arrow (first)
+
+-- 1. Random number generation
+
+fiveRands :: [Integer]
+fiveRands = let f (_, s) = rand s in map fst . take 5 . iterate f . rand . mkSeed $ 1
+
+p1 = product fiveRands
+
+-- 2. Random character generation
+
+randLetter :: Seed -> (Char, Seed)
+-- randLetter = first toLetter . rand
+randLetter = (\(i, s) -> (toLetter i, s)) . rand
+
+randString3 :: String
+randString3 = let f (_, s) = randLetter s in map fst . take 3 . iterate f . randLetter . mkSeed $ 1
+
+-- 3. More generators
+
+type Gen a = Seed -> (a, Seed)
+
+generalA :: (a -> b) -> Gen a -> Gen b
+-- generalA f g = first f . g
+generalA f g = (\(a, b) -> (f a, b)) . g
+
+randEven = generalA (*2) rand
+randOdd = generalA succ randEven
+randTen = generalA (*10) rand
+
+p3 = let seed = mkSeed 1 in product . map (fst . ($ seed)) $ [randEven, randOdd, randTen]
+
+-- 4. Generalizing random pairs
+
+randPair :: Gen (Char, Integer)
+randPair s = ((c, i), ss')
+     where (c, s') = randLetter s
+           (i, ss') = rand s'
+
+
+generalPair :: Gen a -> Gen b -> Gen (a, b)
+generalPair ga gb s = ((a', b') , ss')
+    where (a', s') = ga s
+          (b', ss') = gb s'
+
+generalB :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+generalB f ga gb s = (f a' b', ss')
+    where (a', s') = ga s
+          (b', ss') = gb s'
+
+generalPair2 = generalB (\a b -> (a, b))
+
+p4 = allSame . map ($ mkSeed 1) $ [randPair, generalPair randLetter rand, generalPair2 randLetter rand]
+    where allSame [] = True
+          allSame (x:xs) = all (== x) xs
+
+-- 5. Generalizing lists of generators
+
+repRandom :: [Gen a] -> Gen [a]
+repRandom [] = \s -> ([], s)
+repRandom (x:xs) = generalB (:) x . repRandom $ xs
+
+p5 = (== randString3) . fst . repRandom (replicate 3 randLetter) . mkSeed $ 1
+
+-- 6. Threading the random number state
+
+genTwo :: Gen a -> (a -> Gen b) -> Gen b
+genTwo ga f = uncurry f . ga
+
+mkGen :: a -> Gen a
+mkGen z s = (z, s)
--- src/Set1/RandomNumberGeneration.hs
+++
@@ -1,9 +0,0 @@
-module Set1.RandomCharacterGeneration (fiveRands) where
-
-import MCPrelude
-import Data.List (unfoldr)
-
-fiveRands :: [Integer]
-fiveRands = take 5 . unfoldr (Just . rand) . mkSeed $ 1
-
-p = product fiveRands
